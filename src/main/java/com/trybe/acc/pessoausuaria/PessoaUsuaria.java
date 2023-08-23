b76207254933ecf5fdf7be683750b87489cd7b4c
eon <bernardomp.dev@gmail.com>
2023-08-23 11:10:13 -0300

    feat: PessoaUsuaria Class.

--- src/main/java/com/trybe/acc/pessoausuaria/PessoaUsuaria.java
+++ src/main/java/com/trybe/acc/pessoausuaria/PessoaUsuaria.java
@@ -1,16 +1,29 @@
 package com.trybe.acc.pessoausuaria;
 
+/** Class. */
 public class PessoaUsuaria extends Pessoa {
 
-  public PessoaUsuaria() {
-
+  public PessoaUsuaria(
+      String nome,
+      String sobrenome
+  ) {
+    super(nome, sobrenome);
   }
 
   /**
    * Método para pegar o nome e sobrenome do usuário.
    *
    */
-  public String getPessoaUsuaria() {
-    return null;
+  protected String getPessoaUsuaria() {
+    if (this.nome == null || this.sobrenome == null) {
+      return "Pessoa usuária inválida";
+    }
+
+    if (this.nome.isEmpty() || this.sobrenome.isEmpty()) {
+      return "Pessoa usuária inválida";
+    }
+
+    return super.nome + "." + super.sobrenome;
   }
 }
+
