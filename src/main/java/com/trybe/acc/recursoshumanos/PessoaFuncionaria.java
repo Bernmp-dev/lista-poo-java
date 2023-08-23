86b0a5d5d6b03f315a69c1f034bc9419c2deedd1
eon <bernardomp.dev@gmail.com>
2023-08-23 11:08:11 -0300

    feat: PessoaFuncionaria Class.

--- src/main/java/com/trybe/acc/recursoshumanos/PessoaFuncionaria.java
+++ src/main/java/com/trybe/acc/recursoshumanos/PessoaFuncionaria.java
@@ -4,5 +4,51 @@ package com.trybe.acc.recursoshumanos;
  * Classe que representa uma pessoa funcionária no sistema de recursos humanos.
  */
 public class PessoaFuncionaria {
+  private String nomeCompleto;
+  private final String cpf;
+  private String endereco;
+  private double salario;
 
+  /** Constructor. */
+  public PessoaFuncionaria(
+      String nomeCompleto,
+      String cpf,
+      String endereco,
+      double salario
+  ) {
+    this.nomeCompleto = nomeCompleto;
+    this.cpf = cpf;
+    this.endereco = endereco;
+    this.salario = salario;
+  }
+
+  // Getters
+  public String getNomeCompleto() {
+    return nomeCompleto;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public String getEndereco() {
+    return endereco;
+  }
+
+  public double getSalario() {
+    return salario;
+  }
+
+  // Setters
+  public void setNomeCompleto(String nomeCompleto) {
+    this.nomeCompleto = nomeCompleto;
+  }
+
+  public void setEndereco(String endereco) {
+    this.endereco = endereco;
+  }
+
+  public void setSalario(double salario) {
+    this.salario = salario;
+  }
 }
