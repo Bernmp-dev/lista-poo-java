8ae2cb44344ef5dc47ff2b472a2e9827ba46ef25
eon <bernardomp.dev@gmail.com>
2023-08-23 11:09:53 -0300

    feat: Pessoa Class.

--- src/main/java/com/trybe/acc/pessoausuaria/Pessoa.java
+++ src/main/java/com/trybe/acc/pessoausuaria/Pessoa.java
@@ -1,5 +1,15 @@
 package com.trybe.acc.pessoausuaria;
 
+/** Class. */
 public class Pessoa {
+  protected final String nome;
+  protected final String sobrenome;
 
+  Pessoa(
+      String nome,
+      String sobrenome
+  ) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+  }
 }
